4cebae797f3761977c3a2018dba8619ded219e0b
Szymon Paluch <12szymon.paluch34@gmail.com>
2023-07-10 16:50:58 +0200

    feat: Implement the hashing mechanism

--- src/api.rs
+++ src/api.rs
@@ -2,6 +2,9 @@ use rusqlite::{Connection, params};
 use leptos::{server, ServerFnError};
 use serde::{Serialize, Deserialize};
 
+const SELECT_QUERY: &'static str = "SELECT * FROM urls WHERE hash = ?";
+const INSERT_QUERY: &'static str = "INSERT INTO users (url, hash, used_count) VALUES (?, ?, 0)";
+
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize,)]
 struct Url {
     id: u16,
@@ -11,8 +14,8 @@ struct Url {
 }
 
 pub async fn connect_db() -> Result<Connection, ServerFnError> {
-    let conn = Connection::open("../data.sqlite")?;
-    conn.execute(
+    let connection = Connection::open("../data.sqlite")?;
+    connection.execute(
         "CREATE TABLE IF NOT EXISTS urls (
              id INTEGER PRIMARY KEY,
              url TEXT NOT NULL,
@@ -21,17 +24,16 @@ pub async fn connect_db() -> Result<Connection, ServerFnError> {
          )",
         (),
     )?;
-    return Ok(conn);
+    return Ok(connection);
 }
 
 #[server(GetUrl, "/api", "GetJson")]
 pub async fn get_url(hash: String) -> Result<String, ServerFnError> {
     let connection = connect_db().await?;
     
-    let query = "SELECT * FROM urls WHERE hash = ?";
-    let mut statement = connection.prepare(query)?;
-    let mut rows = statement.query(params![hash])?;
-    let first_row = rows.next()?;
+    let mut statement = connection.prepare(SELECT_QUERY)?;
+    let mut query_result = statement.query(params![hash])?;
+    let first_row = query_result.next()?;
     let url: String = first_row.expect("url not found").get(1)?;
 
     return Ok(url);
@@ -39,17 +41,41 @@ pub async fn get_url(hash: String) -> Result<String, ServerFnError> {
 
 #[server(GenerateUrl, "/api")]
 pub async fn generate_url(input_url: String) -> Result<String, ServerFnError> {
-    // TODO: generate the hash
-    let hash = "";
-
     let connection = connect_db().await?;
-    
-    let query = "INSERT INTO users (url, hash, used_count) VALUES (?, ?, 0)";
-    let mut statement = connection.prepare(query)?;
-    let mut rows = statement.query(params![input_url, hash])?;
-    let first_row = rows.next()?;
+
+    let mut hash = get_random_hash();
+    loop {
+        let mut select_statement = connection.prepare(SELECT_QUERY)?;
+        let mut query_result = select_statement.query(params![&hash])?;
+
+        if query_result.next()?.is_none() {
+            break;
+        }
+
+        hash = get_random_hash();
+    }
+
+    let mut insert_statement = connection.prepare(INSERT_QUERY)?;
+    let mut query_result = insert_statement.query(params![input_url, hash])?;
+    let first_row = query_result.next()?;
     let url: String = first_row.expect("url not found").get(1)?;
 
     return Ok(url);
 }
 
+fn get_random_hash() -> String {
+    use rand::Rng;
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789)(*&^%$#@!~";
+    const HASH_LENGTH: usize = 8;
+    let mut rng = rand::thread_rng();
+
+    let hash: String = (0..HASH_LENGTH)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+    return hash;
+}
