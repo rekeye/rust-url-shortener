fc79a956ee12e98d9882e7d70a257175fb2438f5
Szymon Paluch <12szymon.paluch34@gmail.com>
2023-07-10 14:14:38 +0200

    feat: Database sqlite integration

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use axum::{
     }
 };
 use std::net::SocketAddr;
+use rusqlite::Connection;
 
 mod root;
 mod meta;
@@ -17,6 +18,8 @@ use crate::root::root;
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+
+    let _conn = connect_db().await.expect("couldn't connect to db");
 
     let app = Router::new()
         .route("/", get(root))
@@ -36,4 +39,18 @@ async fn styles() -> impl IntoResponse {
         .header("Content-Type", "text/css")
         .body(include_str!("../style/output.css").to_owned())
         .unwrap();
+}
+
+async fn connect_db() -> Result<Connection, Box<dyn std::error::Error>> {
+    let conn = Connection::open("../data.sqlite")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS urls (
+             id INTEGER PRIMARY KEY,
+             url TEXT NOT NULL,
+             hash TEXT NOT NULL,
+             used_count INTEGER NOT NULL
+         )",
+        (),
+    )?;
+    return Ok(conn);
 }
